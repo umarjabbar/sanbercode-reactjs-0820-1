91e1dc564d5352d7a256fc6ac387d8397417e8dc
umarjabbar <umarjuabbar@gmail.com>
2020-08-27 07:50:49 +0700

    Tugas ketiga (week 1)

---
+++ Tugas-3/tugas3.js
@@ -0,0 +1,71 @@
+***** Soal 1
+var kataPertama = "saya";
+var kataKedua = "senang";
+var kataKetiga = "belajar";
+var kataKeempat = "javascript";
+
+var kedua = kataKedua.replace("s", "S");
+console.log(kataPertama  + " " + kedua + " " + kataKetiga + " " + kataKeempat.toUpperCase());
+
+// ***** Soal 2
+// var kataPertama = "1";
+// var kataKedua = "2";
+// var kataKetiga = "4";
+// var kataKeempat = "5";
+
+// var angkaPertama = Number(kataPertama);
+// var angkaKedua = Number(kataKedua);
+// var angkaKetiga = Number(kataKetiga);
+// var angkaKeempat = Number(kataKeempat);
+// console.log(angkaPertama + angkaKedua + angkaKetiga + angkaKeempat);
+
+// ****** Soal 3
+// var kalimat = 'wah javascript itu keren sekali'; 
+
+// var kataPertama = kalimat.substring(0, 3); 
+// var kataKedua = kalimat.substring(4, 14)
+// var kataKetiga = kalimat.substring(15, 18)
+// var kataKeempat = kalimat.substring(19, 24)
+// var kataKelima = kalimat.substring(25, 31)
+
+// console.log('Kata Pertama: ' + kataPertama); 
+// console.log('Kata Kedua: ' + kataKedua); 
+// console.log('Kata Ketiga: ' + kataKetiga); 
+// console.log('Kata Keempat: ' + kataKeempat); 
+// console.log('Kata Kelima: ' + kataKelima);
+
+// ***** Soal 4
+// var nilai = 99;
+
+// if( nilai >= 80 ){
+//   console.log('A');
+// } else if (nilai < 80 && nilai >= 70 ){
+//   console.log('B');
+// } else if (nilai < 70 && nilai >= 60 ){
+//   console.log('C');
+// } else if (nilai <60 && nilai >= 50 ){
+//   console.log('D')
+// } else {
+//   console.log('E')
+// }
+
+// **** Soal 5
+// var tanggal = 8;
+// var bulan = 2;
+// var tahun = 2002;
+
+// switch(bulan){
+//   case 1: { bulan = 'Januari'; break; }  
+//   case 2: { bulan = 'Februari'; break; }  
+//   case 3: { bulan = 'Maret'; break; }  
+//   case 4: { bulan = 'April'; break; }  
+//   case 5: { bulan = 'Mei'; break; }  
+//   case 6: { bulan = 'Juni'; break; }  
+//   case 7: { bulan = 'Juli'; break; }  
+//   case 8: { bulan = 'Agustus'; break; }  
+//   case 9: { bulan = 'September'; break; }  
+//   case 10: { bulan = 'Oktober'; break; }  
+//   case 11: { bulan = 'November'; break; }  
+//   case 12: { bulan = 'Desember'; break; }  
+// }
+// console.log(tanggal.toString() + " " + bulan + " " + tahun.toString() + " ");
